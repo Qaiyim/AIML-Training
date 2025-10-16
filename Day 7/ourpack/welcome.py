4968fea57648dea641bb51c000c39a54c6db053a
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 12:14:20 +0800

    Create welcome.py

---
+++ Day 7/ourpack/welcome.py
@@ -0,0 +1,11 @@
+# #Firsst way
+# def display():
+#     return 'Welcome to our pack:'
+
+#Second way
+def display(name):
+    print ('Welcome to our pack: ',name)
+
+#Third way
+# def display(username):
+#     return f'Welcome to our pack: {username}'
