c0527ffc95388b97831897bc1a388b7889f4c128
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 12:11:12 +0800

    Create cube.py

---
+++ Day 4/cube.py
@@ -0,0 +1,17 @@
+# print('Write a function to find out the cube of given number')
+# #5: 5*5*5(e.g cube of 5 is 5*5*5 means 125)
+
+# def cube(num):
+#     return num*num*num
+
+# given_num=int(input('Enter number to find out cube of number'))
+# print(f'Number is {given_num} cube is', cube(given_num))
+
+#Write a function to calculate bonus of given salary
+#function take salary as input and return bonus 10% of salary
+
+# def bonus(salary):
+#     return salary*0.1
+
+# salary=float(input('Please enter your salary:\t'))
+# print(f'Your salary is {salary} and bonus is',bonus(salary))
