40fae93e3a764b85999044dc9c6870094dd81291
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 12:11:30 +0800

    Create function_ex.py

---
+++ Day 4/function_ex.py
@@ -0,0 +1,33 @@
+#function without parameters
+# def welcome():
+#     print('Welcome to Functions')
+#     print('Ou First Function')
+
+# welcome()
+# welcome()
+
+#Function with parameter
+# def welcome(name):
+#     print('Welcome to functions in python Mr.\\Ms',name)
+
+# name=input('Enter your name: \t')
+# #Function call
+# welcome(name)
+
+
+#Function with parameter and return (adding)
+# def add(num1,num2):
+#     return num1+num2
+
+# fnum=int(input('Enter your first number:\t'))
+# snum=int(input('Enter your second number:\t'))
+# print(f'Result after adding {fnum} and {snum} is =\t', add(fnum,snum))
+
+
+#Function with parameter and return (multiplication)
+# def multi(num1,num2):
+#     return num1*num2
+
+# fnum=int(input('Enter your first number:\t'))
+# snum=int(input('Enter your second number:\t'))
+# print(f'Result after multiplying {fnum} and {snum} is =\t', multi(fnum,snum))
