4992ed5ffae8e08b85dd1cb9b3e1f37519d8b517
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 12:11:48 +0800

    Create map_ex.py

---
+++ Day 5/map_ex.py
@@ -0,0 +1,10 @@
+# number =[10,25,30,40,2,1]
+# double_number=list(map(lambda n:2*n,number))
+# print('Original List')
+# for num in number:
+#     print(num, end=' ')
+
+# print('\nDouble List\n')
+# for num in double_number:
+#     print(num, end=" ")
+
