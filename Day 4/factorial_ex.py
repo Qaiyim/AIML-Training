7e6b13663f9cc4302631a36bc6cbfd26d82ee1f8
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 12:11:21 +0800

    Create factorial_ex.py

---
+++ Day 4/factorial_ex.py
@@ -0,0 +1,28 @@
+# def factorial(num):
+#     if(num==0) or(num==1):
+#         return 1
+#     else:
+#         return num*factorial (num-1)
+    
+
+# num=int(input('Enter a number to find out factorial:\t'))
+# print(f'Factorial of {num} is: ',factorial(num))
+
+#write a python function which converts inches to cms
+# 1 inch=2.5cm
+
+
+
+# def inch_to_cm(num):
+#     return num*2.5
+
+# num=int(input('Please enter your height in inch:\t'))
+# print(f'Your height in cm is {num} while in inch is',inch_to_cm(num))
+
+
+# def height(cm):
+#     return cm*2.5
+
+# num=int(input('Please enter your height in cm:\t'))
+# print(f'Your height in cm is {num} while in inch is', height(num))
+
