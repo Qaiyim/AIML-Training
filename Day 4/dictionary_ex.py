95082134593c911cd3bf18515b96833d7b8da972
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 12:11:16 +0800

    Create dictionary_ex.py

---
+++ Day 4/dictionary_ex.py
@@ -0,0 +1,39 @@
+# employee={'id':1,
+#           'name':'Qaiyim',
+#           'Salary':7000.50}
+
+# print('Employees Details as follows:')
+# for key,value in employee.items():
+#     print(key, "->", value)
+
+# #Adding key in dictionary
+# employee['city']='Merbok'
+# print('\nDictionary after adding city\n')
+
+# for key, value in employee.items():
+#     print(key, '->', value)
+
+# #Deleting key in dictionary
+# del employee['Salary']
+# print('\nEmployee details after deleting salary\n')
+
+# for key, value in employee.items():
+#     print(key, '->', value)
+
+#To print keys only
+# employee={'id':1,
+#           'name':'Qaiyim',
+#           'Salary':7000.50}
+# print('\nAll keys from Employee\n')
+# for k in employee.keys():
+#     print(k,end='\t')
+
+#To print values only
+# print('\nAll values from Employee\n')
+# for v in employee.values():
+#     print(v,end='\t')
+
+# #To print keys and values beside each other
+# print('\nKey: Value\n')
+# for k, v in employee.items():
+#     print(k,':',v)
