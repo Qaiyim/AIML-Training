9072ec0957c0d55056ca8746ac07408795941e65
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 11:25:15 +0800

    Create ex1.py

---
+++ Day 2/ex1.py
@@ -0,0 +1,67 @@
+# print("First Example")
+# for i in range (10):
+#       print(i)
+#       print("Welcome")
+
+
+# print("Second Example")
+# for i in range (1,11):
+#     print(i)
+
+# print("Third Example")
+# for i in range (10):
+#      print(i+1)  
+
+# for i in range (5):
+#     print (i)
+
+# for i in range (3,5):
+#     print (i)
+
+# for item in range (100):
+#     print (item+1)
+
+# for item in range (100):
+#     print (item+1,end="\t")
+
+# for item in range (100):
+#      print (item+1,end=" ")
+
+# employees=['Raj','Sam', 'Anu', 'Ri']
+# for i in employees:
+#     print(i)
+
+# products=['Facewash','Sunscrean','Facial Kit','Lip Glows','Lip Balm','Hair Oil']
+# for items in products:
+#     print(items)
+
+# for ch in 'Nexperts Academy':
+#     print(ch,end="->")
+
+# for ch in 'Nexperts Academy':
+#     print(ch,end="\t")
+
+# yourName=input("Enter your name:\t")
+# print("Characters of your name as follows:\t")
+# for ch in yourName:
+#     print(ch,end="\t")
+
+# number=int(input("Please enter number to find out table: \t"))
+# print(f"Table of Number {number} as follows \n")
+# for i in range (1,11):
+#     print(f"{number}*{i}=\t{number*i}")
+
+
+# One example of for loop to iterate through tuple
+
+# players=("MSD","VK","KL","RS")
+# for player in players:
+#     print(player)
+
+# players=("Messi","Ronaldo","Pele","Qaiyim")
+# for player in players:
+#      print(player[0])
+
+# players=("Messi","Ronaldo","Pele","Qaiyim")
+# for player in players:
+#      print(player,end=" ")
