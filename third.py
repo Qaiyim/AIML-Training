2e4ed490d16dc4af3ba0263ca3a58e7386dbe254
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 11:23:44 +0800

    Create third.py

---
+++ third.py
@@ -0,0 +1,12 @@
+username="Qaiyim" #string example
+age=26 # integer example
+salary=5000.50 #floating number
+isGraduate=False #Boolean
+stayStatus=None #None
+
+print("Given user Name is: ",username)
+print("Age:", age)
+print("Salary",salary)
+print("Graduate: ", isGraduate)
+print("StayStatus:",stayStatus)
+
