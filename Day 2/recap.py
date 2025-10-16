f90c7904b9f381866d458f7d51b1d7abf366f7c4
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 11:25:23 +0800

    Create recap.py

---
+++ Day 2/recap.py
@@ -0,0 +1,20 @@
+# print("Welcome to Day 2 of Python Journey")
+
+# username="Qaiyim" 
+# print("Welcome:\t",username)
+
+# studentName=input("Enter student name:\t")
+# print("Student name is:\t",studentName)
+
+# studentName=input("Enter student name:\t")
+# print(f"Welcome {studentName}")
+
+# studentName=input("Enter student name:\t")
+# age=int(input("Enter age:\t"))
+# print(f"Welcome {studentName} \nYour details as follows: \nName: {studentName} \nAge: {age}")
+
+# marks=int(input("Enter Marks:\t"))
+# if(marks>=50):
+#     print("Welldone! You have cleared the exam!")
+# else:
+#     print("Sorry! But you have to reattempt it")
