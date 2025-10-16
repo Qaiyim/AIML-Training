4a94f7e5942daafd63a02117c858941c4179e621
Qaiyim <ahmadqaiyim@gmail.com>
2025-10-16 12:10:41 +0800

    Create DictionaryEx.py

---
+++ Day 3/DictionaryEx.py
@@ -0,0 +1,22 @@
+# print("Dictionary Example")
+# our_dictionary=[
+#     {'id':'1','name':'Qaiyim','age':'26'},
+#     {'id':'2','name':'Intan','age':'25'},
+#     {'id':'3','name':'Ahmad','age':'23'},
+#     {'id':'4','name':'Fatehah','age':'22'}
+# ]
+# for keys in our_dictionary:
+#     print(keys)
+
+#example 2
+
+print("Dictionary Example")
+our_dictionary=[
+    {'id':'1','name':'Qaiyim','age':'26'},
+    {'id':'2','name':'Intan','age':'25'},
+    {'id':'3','name':'Ahmad','age':'23'},
+    {'id':'4','name':'Fatehah','age':'22'}
+    ]
+for k in our_dictionary:
+    print(k['id']+'->'+k['name']+'->'+k['age'])
+    
